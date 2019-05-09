f4f2b680c35e8041d6f75972d61b5bd0b5b4a355
yanagi.tomohiro <yanagi.tomohiro@hamee.co.jp>
2019-05-09 11:51:27 +0900

    リモート画面の受け取りをa-frameを利用したVR空間に変更

--- src/front/answer.ts
+++ src/front/answer.ts
@@ -1,4 +1,45 @@
 import ScreenReciever from './screen-reciever';
 
-const remoteVideo = <HTMLVideoElement>document.getElementById('remote-video');
-const reciever = new ScreenReciever(remoteVideo);
+const createVideo = () => {
+  var aVideo = document.createElement('a-video');
+  aVideo.setAttribute('height', '9');
+  aVideo.setAttribute('width', '16');
+  aVideo.setAttribute('position', '0 4 -9');
+  console.log('ATTACH NOW');
+  const videoHolder = document.getElementById('video-holder');
+
+  if(videoHolder == null) {
+    console.error('not find element "#video-holder"');
+    return;
+  }
+
+  videoHolder.appendChild(aVideo);
+
+  const remoteVideo = document.createElement('video');
+  const assets = document.querySelector('a-assets');
+
+  if(assets == null) {
+    return;
+  }
+
+  assets.addEventListener('loadeddata', () => {
+    console.log('loaded asset data');
+  })
+
+  remoteVideo.setAttribute('id', 'remote-video');
+  remoteVideo.setAttribute('autoplay', 'true');
+  remoteVideo.setAttribute('src', '');
+  assets.appendChild(remoteVideo);
+
+  remoteVideo.addEventListener('loadeddata', () => {
+    // Pointing this aframe entity to that video as its source
+    aVideo.setAttribute('src', `#remote-video`);
+  });
+
+  // const remoteVideo = <HTMLVideoElement>document.getElementById('remote-video');
+  const reciever = new ScreenReciever(remoteVideo);
+
+  // remoteVideo.srcObject = stream;
+}
+
+createVideo();
